c5caa21111ab1ad985ab3ea43411da3728a83541
Terrijon Fowler <terrijonfowler@gmail.com>
2016-10-26 09:38:55 -0400

    Update Autoloader.php

--- application/config/Autoloader.php
+++ application/config/Autoloader.php
@@ -21,7 +21,7 @@ function __autoload($className)  {
     foreach( $directorys as $directory ) {
         $classPath = ABS_BASE_PATH . 'application/' . $directory . $className . '.php';
 echo "ClassPath: $classPath<br>";
-        if( file_exists($classPath) || file_exists(strtolower($classPath)) ) {
+        if( file_exists($classPath) ) {
             echo "Including it!<br>";
             include_once $classPath;
 

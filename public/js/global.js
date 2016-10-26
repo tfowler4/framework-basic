5722c1ab81adaf7f8c16fb24df0b0f04addbb8d2
Terrijon Fowler <terrijonfowler@gmail.com>
2016-10-26 12:24:38 -0400

    Update global.js

--- public/js/global.js
+++ public/js/global.js
@@ -2,7 +2,7 @@
     console.log("here");
 
     // for testing switching teplates and ajax calls
-    $('#test-template-selector).parent().find(li a').click(function(e) {
+    $('#test-template-selector').parent().find('li a').click(function(e) {
         e.preventDefault();
 
         console.log('clicked');
